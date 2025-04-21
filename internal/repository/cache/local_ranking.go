b8fe310e7188d5bd6be776b5756cea3bd31aee76
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-04-21 14:32:44 +0800

    榜单查询接口与本地缓存

---
+++ internal/repository/cache/local_ranking.go
@@ -0,0 +1,46 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"github.com/ecodeclub/ekit/syncx/atomicx"
+	"github.com/jym/mywebook/internal/domain"
+	"time"
+)
+
+type RankingLocalCache struct {
+	//可以考虑用uber的缓存
+	topN       *atomicx.Value[[]domain.Article]
+	ddl        *atomicx.Value[time.Time]
+	expiration time.Duration
+}
+
+func NewRankingLocalCache() *RankingLocalCache {
+	return &RankingLocalCache{
+		ddl:  atomicx.NewValueOf[](time.Now()),
+		topN: atomicx.NewValue[[]domain.Article](),
+		//永不过期 或者非常长
+		expiration: time.Minute * 3,
+	}
+}
+
+func (r *RankingLocalCache) Set(ctx context.Context, arts []domain.Article) error {
+	r.topN.Store(arts)
+	ddl := time.Now().Add(r.expiration)
+	r.ddl.Store(ddl)
+	return nil
+}
+
+func (r *RankingLocalCache) Get(ctx context.Context) ([]domain.Article, error) {
+	ddl := r.ddl.Load()
+	arts := r.topN.Load()
+	if len(arts) == 0 || ddl.Before(time.Now()) {
+		return nil, errors.New("已经过期了")
+	}
+	return arts, nil
+}
+
+func (r *RankingLocalCache) ForceGet(ctx context.Context) ([]domain.Article, error) {
+	arts := r.topN.Load()
+	return arts, nil
+}
--- internal/repository/ranking.go
+++ internal/repository/ranking.go
@@ -8,18 +8,39 @@ import (
 
 type RankingRepository interface {
 	ReplaceTopN(ctx context.Context, arts []domain.Article) error
+	GetTopN(ctx context.Context) ([]domain.Article, error)
 }
 
 type rankingRepository struct {
 	cache cache.RankingCache
+	local *cache.RankingLocalCache
+}
+
+func (repo *rankingRepository) GetTopN(ctx context.Context) ([]domain.Article, error) {
+	arts, err := repo.local.Get(ctx)
+	if err == nil {
+		return arts, nil
+	}
+	arts, err = repo.cache.Get(ctx)
+	if err == nil {
+		_ = repo.local.Set(ctx, arts)
+	} else {
+		//redis出现问题了 ，那我们直接从本地缓存读取，不去考虑它是否过期
+		arts, err = repo.local.ForceGet(ctx)
+	}
+	return arts, err
 }
 
 func (repo *rankingRepository) ReplaceTopN(ctx context.Context, arts []domain.Article) error {
+	//1.先操作本地缓存 因为本地缓存几乎不可能失败
+	_ = repo.local.Set(ctx, arts)
 	return repo.cache.Set(ctx, arts)
+
 }
 
-func NewrankingRepository(cache cache.RankingCache) RankingRepository {
+func NewrankingRepository(cache cache.RankingCache, local cache.RankingLocalCache) RankingRepository {
 	return &rankingRepository{
 		cache: cache,
+		local: &local,
 	}
 }
