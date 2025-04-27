d0876b009be7e13f18b18af33832466028dc3c5b
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-04-27 11:38:58 +0800

    数据迁移前期准备

---
+++ interactive/integration/startup/log.go
@@ -0,0 +1,14 @@
+package startup
+
+import (
+	"github.com/jym/mywebook/pkg/logger"
+	"go.uber.org/zap"
+)
+
+func InitLogger() logger.Logger {
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	return logger.NewZapLogger(log)
+}
